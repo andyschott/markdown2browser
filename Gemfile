2e52d588b67d95e859fd42ec5464b47e7cdfbfec
Andy Schott <schottac@gmail.com>
2013-05-16 20:44:49 -0400

    convert markdown to html and show it in the default browser

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'kramdown', '~>  1.0.2'
-
+gem 'launchy',   '~> 2.3.0'
--- markdown2browser.rb
+++ markdown2browser.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'kramdown'
+require 'launchy'
 
 if ARGV.size < 1
   puts "usage here!"
@@ -16,4 +17,14 @@ if !File.exist?(inputPath)
 end
 
 markdown = IO.read(inputPath)
-puts Kramdown::Document.new(markdown, :input => 'markdown').to_html
+
+# Convert the markdown into html
+html = Kramdown::Document.new(markdown, :input => 'markdown').to_html
+
+# Save the generated html to a temporary file
+destFile = "#{ENV['TMPDIR']}#{inputPath}.html"
+IO.write(destFile, html)
+
+# Open the temporary file in the default browser
+Launchy.open destFile
+
